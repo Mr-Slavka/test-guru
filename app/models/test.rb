1aec968cecc1dfb7996a962109ed69d513675521
slavon <vaceslavmartynov325@gmail.com>
2022-06-18 14:18:20 +0300

    feat: add tests_by_category method in Test

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,4 +1,10 @@
-class Test < ApplicationRecord
-  belongs_to :user
-  belongs_to :category
-end
+
+  class Test < ApplicationRecord
+    belongs_to :user
+    belongs_to :category
+
+    def self.tests_by_category(category)
+      category_id = Category.where(title: category).pluck(:id)
+      Test.order(id: :desc).where(category_id: category_id).pluck(:title)
+    end
+  end
