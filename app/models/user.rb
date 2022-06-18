ff96a2a8f56e4a8e2a6390854b47fcf4d70f2bdc
slavon <vaceslavmartynov325@gmail.com>
2022-06-18 22:43:28 +0300

    fix: tests via join

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
 
   def tests(level)
-    test_id = Story.where(user_id: id).pluck(:test_id)
-    pp Test.where(id: test_id, level: level)
+    Test.joins('JOIN stories ON tests.id = stories.test_id')
+        .where('stories.user_id = :id  AND level =  :level', id: id, level: level)
   end
 end
