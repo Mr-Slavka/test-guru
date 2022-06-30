639601737797df6c52a392c92ee1555586cb7247
slavon <vaceslavmartynov325@gmail.com>
2022-06-30 17:26:24 +0300

    fix: changed user_id to author_id in tests

--- db/migrate/20220615145451_create_tests.rb
+++ db/migrate/20220615145451_create_tests.rb
@@ -3,7 +3,7 @@ class CreateTests < ActiveRecord::Migration[6.0]
     create_table :tests do |t|
       t.string :title, null: false
       t.integer :level, default: 0, null: false
-      t.references :user, null: false, foreign_key: true
+      t.references :author, foreign_key: { to_table: :users }, null: false
       t.references :category, null: false, foreign_key: true
 
       t.timestamps
