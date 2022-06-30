0c053b2a99dba57a21cd5926aabdb275ce737ad5
slavon <vaceslavmartynov325@gmail.com>
2022-06-30 17:31:02 +0300

    feat: add associations to models

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,7 +1,12 @@
 
   class Test < ApplicationRecord
-    belongs_to :user
+    belongs_to :author, class_name: 'User', foreign_key: 'author_id'
     belongs_to :category
+
+    has_many :questions, dependent: :destroy
+    has_many :stories, dependent: :destroy
+    has_many :users, through: :stories, dependent: :destroy
+
 
     def self.tests_by_category(category)
       joins('JOIN categories ON tests.category_id = categories.id')
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,9 @@
 class User < ApplicationRecord
 
+  has_many :user_tests, class_name: 'Test', foreign_key: :author_id, dependent: :destroy
+  has_many :stories, dependent: :destroy
+  has_many :tests, through: :stories, dependent: :destroy
+
   def tests(level)
     Test.joins('JOIN stories ON tests.id = stories.test_id')
         .where('stories.user_id = :id  AND level =  :level', id: id, level: level)
