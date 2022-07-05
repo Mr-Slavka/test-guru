a80899576d3f4b5de3495a1d2e1e78bf56d547f0
slavon <vaceslavmartynov325@gmail.com>
2022-07-05 21:00:41 +0300

    feat: add scopes, validations & redid the methods using scopes

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -1,3 +1,14 @@
 class Answer < ApplicationRecord
   belongs_to :question
+
+  validates :body, presence: true
+  validate :validate_count_responses, on: :create
+
+  scope :corrects, -> { where(correct: true) }
+
+  private
+
+  def validate_count_responses
+    errors.add(:number_of_responses, "Questions must have max. 4 answers.") if question.answers.count >= 4
+  end
 end
--- app/models/test.rb
+++ app/models/test.rb
@@ -7,11 +7,18 @@
     has_many :stories, dependent: :destroy
     has_many :users, through: :stories
 
+    validates :title, presence: true,
+                      uniqueness: { scope: %i[level title] }
+    validates :level, numericality: { only_integer: true, greater_than: 0 }
+
+    scope :easy_lvl, -> { where(level: 0..1) }
+    scope :medium_lvl, -> { where(level: 2..4) }
+    scope :hard_lvl, -> { where(level: 5..Float::INFINITY) }
+    scope :by_level, ->(value) { where(level: value) }
+    scope :by_category, ->(category) { joins(:category).order(id: :desc).where(categories: { title: category }) }
+
 
     def self.tests_by_category(category)
-      joins('JOIN categories ON tests.category_id = categories.id')
-        .order(id: :desc)
-        .where('categories.title = :category', category: category)
-        .pluck(:title)
+      by_category(category).pluck(:title)
     end
   end
