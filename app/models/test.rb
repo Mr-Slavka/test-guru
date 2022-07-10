2285f3b82cfc2d1d16102134b51c6669b1c788aa
slavon <vaceslavmartynov325@gmail.com>
2022-07-10 14:19:17 +0300

    fix: small edits to the test

--- app/models/test.rb
+++ app/models/test.rb
@@ -8,17 +8,17 @@
     has_many :users, through: :stories
 
     validates :title, presence: true,
-                      uniqueness: { scope: %i[level title] }
+                      uniqueness: { scope: %i[level] }
     validates :level, numericality: { only_integer: true, greater_than: 0 }
 
-    scope :easy_lvl, -> { where(level: 0..1) }
-    scope :medium_lvl, -> { where(level: 2..4) }
-    scope :hard_lvl, -> { where(level: 5..Float::INFINITY) }
+    scope :easy_lvl, -> { by_level(0..1) }
+    scope :medium_lvl, -> { by_level(2..4) }
+    scope :hard_lvl, -> { by_level(5..Float::INFINITY) }
     scope :by_level, ->(value) { where(level: value) }
-    scope :by_category, ->(category) { joins(:category).order(id: :desc).where(categories: { title: category }) }
+    scope :by_category, ->(category) { joins(:category).where(categories: { title: category }) }
 
 
     def self.tests_by_category(category)
-      by_category(category).pluck(:title)
+      by_category(category).order(title: :desc).pluck(:title)
     end
   end
