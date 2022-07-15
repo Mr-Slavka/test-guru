f8d8846d2c002f1dd665e5d6169dae0c0b0a2252
slavon <vaceslavmartynov325@gmail.com>
2022-07-15 20:25:45 +0300

    feat: add footer

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
-module ApplicationHelper
-end
+
+  module ApplicationHelper
+
+    def current_year
+      DateTime.current.year
+    end
+
+    def github_url(author, repo)
+      link_to 'Github, TestGuru',
+              "https://github.com/#{author}/#{repo}"
+    end
+
+  end
