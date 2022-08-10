3fcec418c12d76d67cac784add1abe7149be8d5c
slavon <vaceslavmartynov325@gmail.com>
2022-08-10 21:31:54 +0300

    fix: add unique true in users email & fix tests_controller

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_08_07_094036) do
+ActiveRecord::Schema.define(version: 2022_08_10_154340) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -69,6 +69,7 @@ ActiveRecord::Schema.define(version: 2022_08_07_094036) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "email", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
   add_foreign_key "answers", "questions"
