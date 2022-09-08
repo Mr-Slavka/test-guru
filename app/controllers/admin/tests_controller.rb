0adb0c78d3735c556f897ca7dc8485705923c996
slavon <vaceslavmartynov325@gmail.com>
2022-09-08 23:58:19 +0300

    fix:small fix in controllers and user models

--- app/controllers/admin/tests_controller.rb
+++ app/controllers/admin/tests_controller.rb
@@ -33,11 +33,6 @@
       redirect_to admin_tests_path
     end
 
-    def start
-      current_user.tests.push(@test)
-      redirect_to current_user.test_passage(@test)
-    end
-
     private
 
     def test_params
