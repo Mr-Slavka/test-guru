0501177ae406dbaaf058f4ee80b976df8f7860ca
slavon <vaceslavmartynov325@gmail.com>
2022-10-10 18:04:58 +0300

    fix:production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -20,13 +20,13 @@ Rails.application.configure do
 
   # Disable serving static files from the `/public` folder by default since
   # Apache or NGINX already handles this.
-  config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
+  config.public_file_server.enabled = true #ENV['RAILS_SERVE_STATIC_FILES'].present?
 
   # Compress CSS using a preprocessor.
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  config.assets.compile = true
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.action_controller.asset_host = 'http://assets.example.com'
@@ -63,6 +63,8 @@ Rails.application.configure do
   config.action_mailer.perform_caching = false
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
+    tls:                  true,
+    domain:               'yandex.ru',
     address:              'smtp.yandex.com',
     port:                 465,
     user_name:            ENV['SMTP_USERNAME'],
