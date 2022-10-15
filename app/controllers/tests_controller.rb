75819e42bcecc10042d858e2b1698746e7413cb3
slavon <vaceslavmartynov325@gmail.com>
2022-10-15 22:37:53 +0300

    fix: test.questions.present?

--- app/controllers/tests_controller.rb
+++ app/controllers/tests_controller.rb
@@ -7,12 +7,7 @@
 
     def start
       @test = Test.find(params[:id])
-      if @test.questions.present?
-        current_user.tests.push(@test)
-        redirect_to current_user.test_passage(@test)
-      else
-        flash_options = { alert: t('.no_questions') }
-        redirect_to tests_path, flash_options
-      end
+      current_user.tests.push(@test)
+      redirect_to current_user.test_passage(@test)
     end
   end
