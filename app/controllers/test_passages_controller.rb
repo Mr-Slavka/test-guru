cbf572c5663757181b060f70adf094b067017f5f
slavon <vaceslavmartynov325@gmail.com>
2022-10-25 23:32:31 +0300

    feat: add timer

--- app/controllers/test_passages_controller.rb
+++ app/controllers/test_passages_controller.rb
@@ -1,6 +1,7 @@
 class TestPassagesController < ApplicationController
 
   before_action :set_test_passage, on: %i[show result update]
+  before_action :check_timer, only: :update
 
   def show; end
 
@@ -24,5 +25,11 @@ class TestPassagesController < ApplicationController
 
   def set_test_passage
     @test_passage = TestPassage.find(params[:id])
+  end
+
+  def check_timer
+    if @test_passage.test.time && @test_passage.overtime?(@test_passage.end_time)
+      redirect_to result_test_passage_path(@test_passage)
+    end
   end
 end
